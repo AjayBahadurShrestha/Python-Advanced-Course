72302cafc33b66c6ecfeb8a7e0fd7a12d2d4586e
Ajay Bahadur Shrestha <blackthunder9849017451@gmail.com>
2026-07-06 13:02:49 +0545

    update timedelta

--- Modules/Date&Time/timedelta1.py
+++ Modules/Date&Time/timedelta1.py
@@ -1,7 +1,8 @@
 from datetime import datetime,timedelta
 today=datetime.now()
 future=today+timedelta(days=7)
-print(future)
+print(f'Today: {today}')
+print(f"The day after 7 days is {future}")
 
 past=today-timedelta(days=3)
-print(past)
+print(f"The day before 3 days is {past}")
