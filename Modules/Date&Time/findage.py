d89f4d85f77b4fa6d65fab0ffa5cb25359c05312
Ajay Bahadur Shrestha <blackthunder9849017451@gmail.com>
2026-07-06 13:36:56 +0545

    complete date and time module

--- Modules/Date&Time/findage.py
+++ Modules/Date&Time/findage.py
@@ -1,5 +1,8 @@
 # Birthday calculator 
 from datetime import datetime
+from dateutil.relativedelta import relativedelta
 birthday= datetime(1992,4,25)
 today=datetime.now()
 print(f"Person age: {today.year-birthday.year} years")
+age= relativedelta(today,birthday)
+print(f"{age.years} Years, {age.months} Months, {age.days} Days")
---
+++ Modules/Date&Time/student_attendance_system.py
@@ -0,0 +1,4 @@
+print("__"*30)
+print("STUDENT ATTENDANCE SYSTEM")
+print("__"*30)
+
