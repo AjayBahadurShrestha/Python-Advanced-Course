226fb156b372cb27b8ad998d32063c89dad25f65
Ajay Bahadur Shrestha <blackthunder9849017451@gmail.com>
2026-07-07 13:26:54 +0545

    Math Module Part-3

---
+++ Modules/Math_Module/log1.py
@@ -0,0 +1,7 @@
+import math as m
+
+print(f'natural log: {m.log(5)}')
+print(f'lob base 10: {m.log10(5)}')
+print(f'lob base 2: {m.log2(5)}')
+print(f'e power of x: {m.exp(1.6094379124341003)}')
+print(f'e power of x-1: {m.expm1(1.6094379124341003)}')
